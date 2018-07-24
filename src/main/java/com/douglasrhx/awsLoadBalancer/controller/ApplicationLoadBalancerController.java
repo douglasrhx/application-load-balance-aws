ce294e48b2a71a4bdd5b8b4827aded775eee4881
Douglas Rocha <douglasrhx@gmail.com>
2018-07-24 09:32:06 -0300

    Adding create methods for other components of load balancer

--- src/main/java/com/douglasrhx/awsLoadBalancer/controller/ApplicationLoadBalancerController.java
+++ src/main/java/com/douglasrhx/awsLoadBalancer/controller/ApplicationLoadBalancerController.java
@@ -13,19 +13,34 @@ import com.douglasrhx.awsLoadBalancer.utils.ApplicationLoadBalancerUtils;
 @RequestMapping(value = "/aws")
 @RestController
 public class ApplicationLoadBalancerController 
-{	
+{
 	@RequestMapping(value = "/loadBalancer", method = RequestMethod.POST)
 	@ResponseBody
-	public void createApplicationLoadBalancer(@RequestBody String body) throws Exception
+	public void createApplicationLoadBalancer(@RequestBody String body) throws Exception 
+	{
+		createRequests(body);
+	}
+
+	private void createRequests(String body) throws Exception 
 	{
 		String TARGET_GROUP_ROOT_NAME = "pools";
+		String TARGET_ROOT_NAME = "target";
+		String RULE_ROOT_NAME = "rule";
+		String LOAD_BALANCER_ROOT_NAME = "loadBalancer";
 		
 		ApplicationLoadBalancerUtils applicationLoadBalancerUtils = new ApplicationLoadBalancerUtils();
-		
 		ApplicationLoadBalancerRequest applicationLoadBalancerRequest = new ApplicationLoadBalancerRequest();
-		
+
 		AbstractALBObject targetGroup = applicationLoadBalancerUtils.recoverObjectFromJson(body, TARGET_GROUP_ROOT_NAME);
-		
 		applicationLoadBalancerRequest.createObjectALB(targetGroup, "CreateTargetGroup");
+		
+		AbstractALBObject target = applicationLoadBalancerUtils.recoverObjectFromJson(body, TARGET_ROOT_NAME);
+		applicationLoadBalancerRequest.createObjectALB(target, "RegisterTargets");
+		
+		AbstractALBObject rule = applicationLoadBalancerUtils.recoverObjectFromJson(body, RULE_ROOT_NAME);
+		applicationLoadBalancerRequest.createObjectALB(rule, "CreateRule");
+		
+		AbstractALBObject loadBalancer = applicationLoadBalancerUtils.recoverObjectFromJson(body, LOAD_BALANCER_ROOT_NAME);
+		applicationLoadBalancerRequest.createObjectALB(loadBalancer, "CreateLoadbalancer");
 	}
 }
